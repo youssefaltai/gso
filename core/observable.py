23cd253f8909b1b80f27907f7e3f99cbfb165686
Youssef Galal <youssefgalalnazem@gmail.com>
2022-10-07 19:37:25 +0200

    Fix: calling _Observable__notify manually
    now users only have to decorate observables' update methods with the "update" decorator.

--- core/observable.py
+++ core/observable.py
@@ -2,24 +2,12 @@ import abc
 
 
 class Observable(metaclass=abc.ABCMeta):
-    def __init__(self, initial_value):
-        self.__value = initial_value
+    def __init__(self):
         self.__observers = []
-
-    @property
-    def value(self):
-        return self.__value
-
-    # FIXME: value setter should not be visible to UI components, only Observable subclasses.
-    @value.setter
-    def value(self, new_value):
-        self.__value = new_value
 
     def attach_observer(self, observer):
         self.__observers.append(observer)
 
-    # FIXME: Users should not need to call self._Observable__notify,
-    #  they should easily select actions to be fired.
     def __notify(self, *actions):
         for observer in self.__observers:
             for action in actions:
--- examples/counter/state/number.py
+++ examples/counter/state/number.py
@@ -1,15 +1,21 @@
+from core import gso
 from core.action import Action
 from core.observable import Observable
 
 
 class Number(Observable):
     def __init__(self, initial_value):
-        Observable.__init__(self, initial_value)
+        Observable.__init__(self)
+        self.__value = initial_value
 
+    @property
+    def value(self):
+        return self.__value
+
+    @gso.update(Action("numberChanged", {}))
     def increment(self, amount):
-        self.value += amount
-        self._Observable__notify(Action("numberChanged", {}))
+        self.__value += amount
 
+    @gso.update(Action("numberChanged", {}))
     def decrement(self, amount):
-        self.value -= amount
-        self._Observable__notify(Action("numberChanged", {}))
+        self.__value -= amount
